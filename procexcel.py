c0112616f2c44ebdbf31a12fc63fde4e7aea818b
alexandre-andreev <alexandre.andreev@gmail.com>
2024-04-04 12:09:58 +0300

    считать данные в фрейм

--- procexcel.py
+++ procexcel.py
@@ -3,3 +3,15 @@
 import pandas as pd
 import os
 
+# каталог хранения EXCRL файлов
+cwd = os.getcwd()
+
+# каталог где хранятся файлы с данными парных сравнений
+os.chdir('D:\\_project\excel-repo')
+
+# название файла парных сравнений
+file = 'compare-tables.xlsx'
+xl = pd.ExcelFile(file)
+
+# открытие листа "Лист1" (имеет нулевой индекс)
+df1 = xl.parse(xl.sheet_names[0])
