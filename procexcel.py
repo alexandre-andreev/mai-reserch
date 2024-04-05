e3b34e70b9141cc7567de6c9cc9018c1d81f99ce
alexandre-andreev <alexandre.andreev@gmail.com>
2024-04-05 12:05:19 +0300

    MAI-6

--- procexcel.py
+++ procexcel.py
@@ -3,15 +3,26 @@
 import pandas as pd
 import os
 
-# каталог хранения EXCRL файлов
-cwd = os.getcwd()
+# каталог хранения EXCEL файлов
+# cwd = os.getcwd()
 
 # каталог где хранятся файлы с данными парных сравнений
 os.chdir('D:\\_project\excel-repo')
 
 # название файла парных сравнений
 file = 'compare-tables.xlsx'
-xl = pd.ExcelFile(file)
+xbook = pd.ExcelFile(file)
+# df = pd.read_excel(file)
+# print(df.head())
 
+# делаем справочник листов в файле
 # открытие листа "Лист1" (имеет нулевой индекс)
-df1 = xl.parse(xl.sheet_names[0])
+df1 = xbook.parse(xbook.sheet_names[0])
+
+data_sheets = {}
+for sheets in xbook.sheet_names:
+    data_sheets[sheets] = xbook.parse(sheets)
+print(data_sheets['Лист1'])
+print(data_sheets['Лист2'])
+print(data_sheets['Лист3'])
+print(data_sheets['Лист1'].shape) # размерность таблицы с данными
